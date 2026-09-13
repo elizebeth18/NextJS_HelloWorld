0a04d232acfaaacdd4400af5917f2525a68e85b9
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-09-13 20:11:02 +0530

    static and dynamic meta data

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,14 +1,25 @@
-const ProductDetail = async ({ params }: {
+import { Metadata } from "next";
+
+type Props = {
     params: Promise<{ productId: string }>
+};
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const { productId } = await params
+    return {
+        title: `Product ${productId}`
+    }
+}
+
+const ProductDetail = async ({ params }: { params: Promise<{ productId: string }>
 }) => {
 
     const productId = (await params).productId
     return (
         <>
-            
             <h1>Details about Product {productId}</h1>
         </>
     )
 }
 
-export default ProductDetail
+export default ProductDetail;
